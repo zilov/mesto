afc15a5ef05b43d0bd5effc7e174bcab13974653
Danil Zilov <zilov.d@gmail.com>
2022-02-14 14:48:45 +0300

    fix: add form variable and event resetter in change profile func

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,7 @@ const profileName = document.querySelector('.profile__name');
 const profileStatus = document.querySelector('.profile__status');
 const popupInputName = popupElement.querySelector('input[name="profile-name"]');
 const popupInputStatus = popupElement.querySelector('input[name="profile-status"]');
+const popupForm = popupElement.querySelector('form[name="form-edit-profile-info"]')
 
 function setInputValues() {
     popupInputName.value = profileName.textContent;
@@ -31,13 +32,16 @@ function closePopup() {
 // popupElement.addEventListener('click', closePopupByBgClick);
 
 
-function changeProfileInfo() {
+function changeProfileInfo(event) {
+    event.preventDefault();
     profileName.textContent = popupInputName.value;
     profileStatus.textContent = popupInputStatus.value;
 
     closePopup();
 };
 
+
+
 editProfileBtn.addEventListener('click', openPopup);
 popupCloseBtn.addEventListener('click', closePopup);
-popupElement.querySelector('form[name="form-edit-profile-info"]').addEventListener('submit', changeProfileInfo);
+popupForm.addEventListener('submit', changeProfileInfo);
