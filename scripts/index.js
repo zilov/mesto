a64a7e80ae2932e08ebd27abfa8177196f85c602
Danil Zilov <zilov.d@gmail.com>
2022-02-17 18:58:04 +0300

    feat: initial cards creating script

--- scripts/index.js
+++ scripts/index.js
@@ -5,12 +5,42 @@ const profileName = document.querySelector('.profile__name');
 const profileStatus = document.querySelector('.profile__status');
 const popupInputName = popupElement.querySelector('input[name="profile-name"]');
 const popupInputStatus = popupElement.querySelector('input[name="profile-status"]');
-const popupForm = popupElement.querySelector('form[name="form-edit-profile-info"]')
+const popupForm = popupElement.querySelector('form[name="form-edit-profile-info"]');
+const cardsContainer = document.querySelector('.cards');
+
+const cardsListInitial = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
 
 function setInputValues() {
     popupInputName.value = profileName.textContent;
     popupInputStatus.value = profileStatus.textContent;
 };
+
 
 function openPopup() {
     setInputValues();
@@ -23,15 +53,6 @@ function closePopup() {
 }
 
 
-// function closePopupByBgClick(event) {
-//     if (event.target !== event.currentTarget) {
-//         return;
-//     }
-//     closePopup();
-// };
-// popupElement.addEventListener('click', closePopupByBgClick);
-
-
 function changeProfileInfo(event) {
     event.preventDefault();
     profileName.textContent = popupInputName.value;
@@ -40,8 +61,20 @@ function changeProfileInfo(event) {
     closePopup();
 };
 
+function addCard(cardTitle='Тест-лягушонок', cardImageLink='../images/card_test_image.jpg') {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = cardImageLink;
+    cardElement.querySelector('.card__title').textContent = cardTitle;
+
+    cardsContainer.append(cardElement);
+}
+
 
 
 editProfileBtn.addEventListener('click', openPopup);
 popupCloseBtn.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', changeProfileInfo);
+
+cardsListInitial.forEach(item => addCard(item.name, item.link));
