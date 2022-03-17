fe759906c0d52b13ca23dd24db0462d661e69860
Danil <zilov.d@gmail.com>
2022-03-17 11:47:42 +0300

    format project files

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,29 +1,36 @@
 const validationSettings = {
-  formSelector: '.form',
-  inputSelector: '.form__input',
-  submitButtonSelector: '.form__submit-btn',
-  inactiveButtonClass: 'form__submit-btn_inactive',
-  inputErrorClass: 'form__input-err',
-  errorClass: 'form__input-err_visible',
-  
-}
+  formSelector: ".form",
+  inputSelector: ".form__input",
+  submitButtonSelector: ".form__submit-btn",
+  inactiveButtonClass: "form__submit-btn_inactive",
+  inputErrorClass: "form__input-err",
+  errorClass: "form__input-err_visible",
+};
 
-const showErrMessage = (formElement, inputElement, errMessage, errActiveClass) => {
+const showErrMessage = (
+  formElement,
+  inputElement,
+  errMessage,
+  errActiveClass
+) => {
   const errMsgElement = formElement.querySelector(`#${inputElement.id}-Error`);
   errMsgElement.textContent = errMessage;
   errMsgElement.classList.add(errActiveClass);
-}
-
+};
 
 const hideErrMessage = (formElement, inputElement, errActiveClass) => {
   const errMsgElement = formElement.querySelector(`#${inputElement.id}-Error`);
-  
-  errMsgElement.textContent = '';
+
+  errMsgElement.textContent = "";
   errMsgElement.classList.remove(errActiveClass);
-}
+};
 
-
-const toggleSubmitBtn = (formElement, formInputs, submitBtnSelector, submitBtnDisabledClass) => {
+const toggleSubmitBtn = (
+  formElement,
+  formInputs,
+  submitBtnSelector,
+  submitBtnDisabledClass
+) => {
   const submitBtn = formElement.parentElement.querySelector(submitBtnSelector);
 
   if (!formInputs.every(isInputValid)) {
@@ -33,21 +40,30 @@ const toggleSubmitBtn = (formElement, formInputs, submitBtnSelector, submitBtnDi
     submitBtn.classList.remove(submitBtnDisabledClass);
     submitBtn.disabled = false;
   }
-}
+};
 
 const isInputValid = (inputElement) => {
   return inputElement.validity.valid;
-}
+};
 
+const addFormValidityListeners = (
+  formElement,
+  formInputs,
+  errActiveClass,
+  submitBtnSelector,
+  submitBtnDisabledClass
+) => {
+  toggleSubmitBtn(
+    formElement,
+    formInputs,
+    submitBtnSelector,
+    submitBtnDisabledClass
+  );
 
-const addFormValidityListeners = (formElement, formInputs, errActiveClass, submitBtnSelector, submitBtnDisabledClass) => {
-  
-  toggleSubmitBtn(formElement, formInputs, submitBtnSelector, submitBtnDisabledClass);
-  
-  formInputs.forEach(input => {
-    input.addEventListener('input', function (event) {
+  formInputs.forEach((input) => {
+    input.addEventListener("input", function (event) {
       const valid = event.target.validity.valid;
-  
+
       if (valid) {
         hideErrMessage(formElement, event.target, errActiveClass);
       } else {
@@ -55,24 +71,34 @@ const addFormValidityListeners = (formElement, formInputs, errActiveClass, submi
         showErrMessage(formElement, event.target, errMessage, errActiveClass);
       }
 
-      toggleSubmitBtn(formElement, formInputs, submitBtnSelector, submitBtnDisabledClass);
-    })
-  })
-}
-
+      toggleSubmitBtn(
+        formElement,
+        formInputs,
+        submitBtnSelector,
+        submitBtnDisabledClass
+      );
+    });
+  });
+};
 
 const enableValidation = (settings) => {
   const forms = Array.from(document.querySelectorAll(settings.formSelector));
-  forms.forEach(formElement => {
-
-    const formInputs = Array.from(formElement.querySelectorAll(settings.inputSelector));
-    formElement.addEventListener('submit', function(event) {
+  forms.forEach((formElement) => {
+    const formInputs = Array.from(
+      formElement.querySelectorAll(settings.inputSelector)
+    );
+    formElement.addEventListener("submit", function (event) {
       event.preventDefault();
-    })
+    });
 
-
-    addFormValidityListeners(formElement, formInputs, settings.errorClass, settings.submitButtonSelector, settings.inactiveButtonClass);
-  })
-}
+    addFormValidityListeners(
+      formElement,
+      formInputs,
+      settings.errorClass,
+      settings.submitButtonSelector,
+      settings.inactiveButtonClass
+    );
+  });
+};
 
 enableValidation(validationSettings);
