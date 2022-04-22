2f78931ddd27b67eadbf07cb82a275deccdcaf0a
Danil <zilov.d@gmail.com>
2022-04-22 18:50:01 +0300

    refactor: card elements

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,36 +3,32 @@ export default class Card {
     this._title = cardData.name,
     this._link = cardData.link,
     this._name = cardData.name,
+
     this._template = document.querySelector(cardSettings.cardTemplateSelector),
-    this._element = this._template.content.cloneNode(true),
+    this._element = this._template.content.querySelector(cardSettings.cardSelector).cloneNode(true),
     this._cardImageElement = this._element.querySelector(cardSettings.cardImageSelector),
     this._cardTitleElement = this._element.querySelector(cardSettings.cardTitleSelector),
-    this._removeButton = cardSettings.cardRemoveBtnSelector,
-    this._likeButton = cardSettings.cardLikeBtnSelector,
-    this._likeActive = cardSettings.cardLikeActiveClass,
-    this._cardSelector = cardSettings.cardSelector,
+    this._removeButton = this._element.querySelector(cardSettings.cardRemoveBtnSelector),
+    this._likeButton = this._element.querySelector(cardSettings.cardLikeBtnSelector),
+    this._likeActiveClass = cardSettings.cardLikeActiveClass,
+
     this._handleCardClick = handleCardClick,
     this._removeCard = this._removeCard.bind(this),
     this._toggleLike = this._toggleLike.bind(this)
   }
   
-  _toggleLike(event) {
-    event.target.classList.toggle(this._likeActive);
+  _toggleLike() {
+    this._likeButton.classList.toggle(this._likeActiveClass);
   }
 
-  _removeCard(event) {
-    event.target.closest(this._cardSelector).remove();
+  _removeCard() {
+    this._element.remove();
   }  
   
   _addCardListeners() {
-    this._element
-    .querySelector(this._removeButton)
-    .addEventListener("click", this._removeCard);
-    this._element
-    .querySelector(this._likeButton)
-    .addEventListener("click", this._toggleLike);
+    this._removeButton.addEventListener("click", this._removeCard);
+    this._likeButton.addEventListener("click", this._toggleLike);
     this._cardImageElement.addEventListener('click', () => {
-      console.log(this._link, this._name);
       this._handleCardClick(this._link, this._name);
     })
   }
